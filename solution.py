a8d7654c2103a8dc120e89acce0a9e515ce10a90
catinabox1 <71033355+catinabox1@users.noreply.github.com>
2023-04-06 16:21:54 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,9 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 417796486 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    y = exp(x - 315)
+    y = sum(y) / len(x)
+    return y # Ваш ответ
