dd96cbfebe8bfa86e325035dc8e42199765f0d2f
catinabox1 <71033355+catinabox1@users.noreply.github.com>
2023-04-07 21:50:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,6 +5,6 @@ import numpy as np
 chat_id = 417796486 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    y = exp(x - 315)
-    y = sum(y) / len(x)
+    y = np.log(x - 315)
+    y = sum(y) / len(y)
     return y # Ваш ответ
